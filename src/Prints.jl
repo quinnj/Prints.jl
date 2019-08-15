5427870801ccd3edeb72e53be2a3b07d4b189322
Jacob Quinn <quinn.jacobd@gmail.com>
2019-08-14 21:17:53 -0600

    trying out no type param for Spec

--- src/Prints.jl
+++ src/Prints.jl
@@ -4,14 +4,14 @@ using Ryu
 
 export @printf, @sprintf, Format, format, @format
 
-const Ints = Union{Val{'d'}, Val{'i'}, Val{'u'}, Val{'x'}, Val{'X'}, Val{'o'}}
-const Floats = Union{Val{'e'}, Val{'E'}, Val{'f'}, Val{'F'}, Val{'g'}, Val{'G'}, Val{'a'}, Val{'A'}}
-const Chars = Union{Val{'c'}, Val{'C'}}
-const Strings = Union{Val{'s'}, Val{'S'}}
-const Pointer = Val{'p'}
-const HexBases = Union{Val{'x'}, Val{'X'}, Val{'a'}, Val{'A'}}
+isint(x) = x == 'd' || x == 'i' || x == 'u' || x == 'x' || x == 'X' || x == 'o'
+isfloat(x) = x == 'e' || x == 'E' || x == 'f' || x == 'F' || x == 'g' || x == 'G' || x == 'a' || x == 'A'
+ischar(x) = x == 'c' || x == 'C'
+isstring(x) = x == 's' || x == 'S'
+ishexbase(x) = x == 'x' || x == 'X' || x == 'a' || x == 'A'
 
-struct Spec{T} # T => %type => Val{'type'}
+struct Spec
+    type::Char
     leftalign::Bool
     plus::Bool
     space::Bool
@@ -21,25 +21,22 @@ struct Spec{T} # T => %type => Val{'type'}
     precision::Int
 end
 
-Base.string(f::Spec{T}; bigfloat::Bool=false) where {T} =
+Base.string(f::Spec; bigfloat::Bool=false) =
     string("%", f.leftalign ? "-" : "", f.plus ? "+" : "", f.space ? " " : "",
         f.zero ? "0" : "", f.hash ? "#" : "", f.width > 0 ? f.width : "",
-        f.precision == 0 ? ".0" : f.precision > 0 ? ".$(f.precision)" : "", bigfloat ? "R" : "", char(T))
-Base.show(io::IO, f::Spec) = print(io, string(f))
+        f.precision == 0 ? ".0" : f.precision > 0 ? ".$(f.precision)" : "", bigfloat ? "R" : "", f.type)
+Base.show(io::IO, f::Spec) = print(io, '"', string(f), '"')
 
-ptrfmt(s::Spec{T}, x) where {T} =
-    Spec{Val{'x'}}(s.leftalign, s.plus, s.space, s.zero, true, s.width, sizeof(x) == 8 ? 16 : 8)
+ptrfmt(s::Spec, x) =
+    Spec('x', s.leftalign, s.plus, s.space, s.zero, true, s.width, sizeof(x) == 8 ? 16 : 8)
 
-struct Format{S, T}
+struct Format{S}
     str::S
-    substrings::Vector{UnitRange{Int}}
-    formats::T # Tuple of Specs
+    substringranges::Vector{UnitRange{Int}}
+    formats::Vector{Spec}
 end
 
-base(::Type{T}) where {T <: HexBases} = 16
-base(::Type{Val{'o'}}) = 8
-base(x) = 10
-char(::Type{Val{c}}) where {c} = c
+base(x) = x in HEXBASES ? 16 : x == 'o' ? 8 : 10
 
 # parse format string
 function Format(f::AbstractString)
@@ -54,7 +51,7 @@ function Format(f::AbstractString)
         (pos > len || (b == UInt8('%') && pos <= len && bytes[pos] != UInt8('%'))) && break
     end
     strs = [1:pos - 1 - (b == UInt8('%'))]
-    fmts = []
+    fmts = Spec[]
     while pos <= len
         b = bytes[pos]
         pos += 1
@@ -123,17 +120,17 @@ function Format(f::AbstractString)
         end
         # parse type
         !(b in b"diouxXDOUeEfFgGaAcCsSpn") && throw(ArgumentError("invalid format string: '$f', invalid type specifier: '$(Char(b))'"))
-        type = Val{Char(b)}
-        if type <: Ints && precision > 0
+        type = Char(b)
+        if isint(type) && precision > 0
             zero = false
-        elseif (type <: Strings || type <: Chars) && !parsedprecdigits
+        elseif (isstring(type) || ischar(type)) && !parsedprecdigits
             precision = -1
-        elseif type <: Union{Val{'a'}, Val{'A'}} && !parsedprecdigits
+        elseif (type == 'a' || type == 'A') && !parsedprecdigits
             precision = -1
-        elseif type <: Floats && !parsedprecdigits
+        elseif isfloat(type) && !parsedprecdigits
             precision = 6
         end
-        push!(fmts, Spec{type}(leftalign, plus, space, zero, hash, width, precision))
+        push!(fmts, Spec(type, leftalign, plus, space, zero, hash, width, precision))
         start = pos
         prevperc = false
         while pos <= len
@@ -153,7 +150,7 @@ function Format(f::AbstractString)
         end
         push!(strs, start:pos - 1 - (b == UInt8('%')))
     end
-    return Format(bytes, strs, Tuple(fmts))
+    return Format(bytes, strs, fmts)
 end
 
 macro format_str(str)
@@ -175,7 +172,7 @@ const HEX = b"0123456789ABCDEF"
     return pos
 end
 
-@inline function fmt(buf, pos, arg, spec::Spec{T}) where {T <: Chars}
+function fmtchars(buf, pos, arg, spec)
     leftalign, width = spec.leftalign, spec.width
     if !leftalign && width > 1
         for _ = 1:(width - 1)
@@ -194,7 +191,7 @@ end
 end
 
 # strings
-@inline function fmt(buf, pos, arg, spec::Spec{T}) where {T <: Strings}
+function fmtstrings(buf, pos, arg, spec)
     leftalign, hash, width, prec = spec.leftalign, spec.hash, spec.width, spec.precision
     str = string(arg)
     op = p = prec == -1 ? (length(str) + (hash ? arg isa AbstractString ? 2 : 1 : 0)) : prec
@@ -234,16 +231,16 @@ end
 end
 
 # integers
-@inline function fmt(buf, pos, arg, spec::Spec{T}) where {T <: Ints}
-    leftalign, plus, space, zero, hash, width, prec =
-        spec.leftalign, spec.plus, spec.space, spec.zero, spec.hash, spec.width, spec.precision
-    bs = base(T)
+function fmtints(buf, pos, arg, spec)
+    type, leftalign, plus, space, zero, hash, width, prec =
+        spec.type, spec.leftalign, spec.plus, spec.space, spec.zero, spec.hash, spec.width, spec.precision
+    bs = base(type)
     arg2 = arg isa AbstractFloat ? Integer(trunc(arg)) : arg
     n = i = ndigits(arg2, base=bs, pad=1)
     x, neg = arg2 < 0 ? (-arg2, true) : (arg2, false)
     arglen = n + (neg || (plus | space)) +
-        (T == Val{'o'} && hash ? 2 : 0) +
-        (T == Val{'x'} && hash ? 2 : 0) + (T == Val{'X'} && hash ? 2 : 0)
+        (type == 'o' && hash ? 2 : 0) +
+        (type == 'x' && hash ? 2 : 0) + (type == 'X' && hash ? 2 : 0)
     arglen2 = arglen < width && prec > 0 ? arglen + min(max(0, prec - n), width - arglen) : arglen
     if !leftalign && !zero && arglen2 < width
         # pad left w/ spaces
@@ -259,15 +256,15 @@ end
     elseif space
         buf[pos] = UInt8(' '); pos += 1
     end
-    if T == Val{'o'} && hash
+    if type == 'o' && hash
         buf[pos] = UInt8('0')
         buf[pos + 1] = UInt8('o')
         pos += 2
-    elseif T == Val{'x'} && hash
+    elseif type == 'x' && hash
         buf[pos] = UInt8('0')
         buf[pos + 1] = UInt8('x')
         pos += 2
-    elseif T == Val{'X'} && hash
+    elseif type == 'X' && hash
         buf[pos] = UInt8('0')
         buf[pos + 1] = UInt8('X')
         pos += 2
@@ -288,18 +285,24 @@ end
             pos += 1
         end
     end
-    while i > 0
-        @inbounds buf[pos + i - 1] = bs == 16 ?
-            (T == Val{'x'} ? hex[(x & 0x0f) + 1] : HEX[(x & 0x0f) + 1]) :
-            (48 + (bs == 8 ? (x & 0x07) : rem(x, 10)))
-        if bs == 8
-            x >>= 3
-        elseif bs == 16
+    if bs == 16
+        while i > 0
+            buf[pos + i - 1] = type == 'x' ? hex[(x & 0x0f) + 1] : HEX[(x & 0x0f) + 1]
             x >>= 4
-        else
-            x = oftype(x, div(x, 10))
+            i -= 1
         end
-        i -= 1
+    elseif bs == 8
+        while i > 0
+            buf[pos + i - 1] = 48 + (x & 0x07)
+            x >>= 3
+            i -= 1
+        end
+    else
+        while i > 0
+            buf[pos + i - 1] = 48 + rem(x, 10)
+            x = oftype(x, div(x, 10))
+            i -= 1
+        end
     end
     pos += n
     if leftalign && arglen2 < width
@@ -313,26 +316,30 @@ end
 end
 
 # floats
-@inline function fmt(buf, pos, arg, spec::Spec{T}) where {T <: Floats}
-    leftalign, plus, space, zero, hash, width, prec =
-        spec.leftalign, spec.plus, spec.space, spec.zero, spec.hash, spec.width, spec.precision
+exp(a, b, c, d, e, f, g, h, i) = Ryu.writeexp(a, b, c, d, e, f, g, h, i)
+fixed(a, b, c, d, e, f, g, h) = Ryu.writefixed(a, b, c, d, e, f, g, h)
+shortest(a, b, c, d, e, f, g, h, i, j) = Ryu.writeshortest(a, b, c, d, e, f, g, h, i, j)
+
+function fmtfloats(buf, pos, arg, spec)
+    type, leftalign, plus, space, zero, hash, width, prec =
+        spec.type, spec.leftalign, spec.plus, spec.space, spec.zero, spec.hash, spec.width, spec.precision
+    x = Float64(arg)
     if arg isa BigFloat && !isnan(arg) && isfinite(arg)
-        lng = ccall((:mpfr_snprintf, :libmpfr), Int32,
+        newpos = ccall((:mpfr_snprintf, :libmpfr), Int32,
                 (Ptr{UInt8}, Culong, Ptr{UInt8}, Ref{BigFloat}),
                 buf, length(buf), string(spec, bigfloat=true), arg)
-        lng > 0 || error("invalid printf formatting for BigFloat")
-        return lng + 1
+        newpos > 0 || error("invalid printf formatting for BigFloat")
+        return newpos + 1
     end
-    x = Float64(arg)
-    if T <: Union{Val{'e'}, Val{'E'}}
-        newpos = Ryu.writeexp(buf, pos, x, plus, space, hash, prec, char(T), UInt8('.'))
-    elseif T <: Union{Val{'f'}, Val{'F'}}
-        newpos = Ryu.writefixed(buf, pos, x, plus, space, hash, prec, UInt8('.'))
-    elseif T <: Union{Val{'g'}, Val{'G'}}
+    if type == 'e' || type == 'E'
+        newpos = exp(buf, pos, x, plus, space, hash, prec, UInt8(type), UInt8('.'))
+    elseif type == 'f' || type == 'F'
+        newpos = fixed(buf, pos, x, plus, space, hash, prec, UInt8('.'))
+    elseif type == 'g' || type == 'G'
         prec = prec == 0 ? 1 : prec
         x = round(x, sigdigits=prec)
-        newpos = Ryu.writeshortest(buf, pos, x, plus, space, hash, prec, T == Val{'g'} ? UInt8('e') : UInt8('E'), true, UInt8('.'))
-    elseif T <: Union{Val{'a'}, Val{'A'}}
+        newpos = shortest(buf, pos, x, plus, space, hash, prec, type == 'g' ? UInt8('e') : UInt8('E'), true, UInt8('.'))
+    elseif type == 'a' || type == 'A'
         x, neg = x < 0 ? (-x, true) : (x, false)
         newpos = pos
         if neg
@@ -358,7 +365,8 @@ end
         else
             buf[newpos] = UInt8('0')
             newpos += 1
-            buf[newpos] = T <: Val{'a'} ? UInt8('x') : UInt8('X')
+            low = type == 'a'
+            buf[newpos] = low ? UInt8('x') : UInt8('X')
             newpos += 1
             if arg == 0
                 buf[newpos] = UInt8('0')
@@ -370,7 +378,7 @@ end
                         prec -= 1
                     end
                 end
-                buf[newpos] = T <: Val{'a'} ? UInt8('p') : UInt8('P')
+                buf[newpos] = low ? UInt8('p') : UInt8('P')
                 buf[newpos + 1] = UInt8('+')
                 buf[newpos + 2] = UInt8('0')
             else
@@ -391,7 +399,7 @@ end
                 end
                 frac = u > 9 || hash || prec > 0
                 while i > 1
-                    buf[newpos + i] = T == Val{'a'} ? hex[(u & 0x0f) + 1] : HEX[(u & 0x0f) + 1]
+                    buf[newpos + i] = low ? hex[(u & 0x0f) + 1] : HEX[(u & 0x0f) + 1]
                     u >>= 4
                     i -= 1
                     prec -= 1
@@ -399,14 +407,14 @@ end
                 if frac
                     buf[newpos + 1] = UInt8('.')
                 end
-                buf[newpos] = T == Val{'a'} ? hex[(u & 0x0f) + 1] : HEX[(u & 0x0f) + 1]
+                buf[newpos] = low ? hex[(u & 0x0f) + 1] : HEX[(u & 0x0f) + 1]
                 newpos += n + frac
                 while prec > 0
                     buf[newpos] = UInt8('0')
                     newpos += 1
                     prec -= 1
                 end
-                buf[newpos] = T <: Val{'a'} ? UInt8('p') : UInt8('P')
+                buf[newpos] = low ? UInt8('p') : UInt8('P')
                 newpos += 1
                 p -= 1
                 buf[newpos] = p < 0 ? UInt8('-') : UInt8('+')
@@ -434,16 +442,16 @@ end
             # right aligned
             n = width - (newpos - pos)
             if zero
-                ex = (arg < 0 || (plus | space)) + (T <: Union{Val{'a'}, Val{'A'}} ? 2 : 0)
+                ex = (arg < 0 || (plus | space)) + (type == 'a' || type == 'A' ? 2 : 0)
                 so = pos + ex
                 len = (newpos - pos) - ex
-                unsafe_copyto!(buf, so + n, buf, so, len)
+                copyto!(buf, so + n, buf, so, len)
                 for i = so:(so + n - 1)
                     buf[i] = UInt8('0')
                 end
                 newpos += n
             else
-                unsafe_copyto!(buf, pos + n, buf, pos, newpos - pos)
+                copyto!(buf, pos + n, buf, pos, newpos - pos)
                 for i = pos:(pos + n - 1)
                     buf[i] = UInt8(' ')
                 end
@@ -454,92 +462,89 @@ end
     return newpos
 end
 
-# pointers
-fmt(buf, pos, arg, spec::Spec{Pointer}) = fmt(buf, pos, Int(arg), ptrfmt(spec, arg))
-
-@inline function format(buf::Vector{UInt8}, pos::Integer, f::Format, args...)
-    # write out first substring
-    for i in f.substrings[1]
-        @inbounds buf[pos] = f.str[i]
+@inline function format(buf::Vector{UInt8}, pos::Integer, f::Format, args)
+    s = f.str
+    fmts = f.formats
+    for i = 1:length(args)
+        # write out any prefix substring
+        for j = f.substringranges[i]
+            buf[pos] = s[j]
+            pos += 1
+        end
+        # write out formatted arg
+        fmt = fmts[i]
+        arg = args[i]
+        if isint(fmt.type)
+            pos = fmtints(buf, pos, arg, fmt)
+        elseif isfloat(fmt.type)
+            pos = fmtfloats(buf, pos, arg, fmt)
+        elseif isstring(fmt.type)
+            pos = fmtstrings(buf, pos, arg, fmt)
+        elseif ischar(fmt.type)
+            pos = fmtchars(buf, pos, arg, fmt)
+        elseif fmt.type == 'p'
+            pos = fmtints(buf, pos, Int(arg), ptrfmt(fmt, arg))
+        else
+            throw(ArgumentError("invalid format type specifier: $(fmt)"))
+        end
+    end
+    # write out any final substring postfix
+    for i = f.substringranges[end]
+        buf[pos] = s[i]
         pos += 1
-    end
-    # for each format, write out arg and next substring
-    # unroll up to 8 formats
-    N = length(f.formats)
-    Base.@nexprs 8 i -> begin
-        if N >= i
-            pos = fmt(buf, pos, args[i], f.formats[i])
-            for j in f.substrings[i + 1]
-                buf[pos] = f.str[j]
-                pos += 1
-            end
-        end
-    end
-    if N > 8
-        for i = 9:length(f.formats)
-            pos = fmt(buf, pos, args[i], f.formats[i])
-            for j in f.substrings[i + 1]
-                @inbounds buf[pos] = f.str[j]
-                pos += 1
-            end
-        end
     end
     return pos
 end
 
-plength(f::Spec{T}, x::Integer) where {T} = max(f.width, f.precision, plength(x)) + plength(T) + f.hash + (x < 0 || (f.plus | f.space))
-plength(f::Spec{T}, x::Real) where {T} = max(f.width, plength(x)) + plength(T) + f.hash + (x < 0 || (f.plus | f.space)) + f.precision
-plength(f, x::AbstractString) = max(f.width, min(f.precision == -1 ? sizeof(x) : f.precision, sizeof(x))) + (sizeof(x) - length(x))
-plength(f, x) = max(f.width, plength(x))
-
-plength(::Type{T}) where {T <: Union{Val{'o'}, Val{'x'}, Val{'X'}}} = 2
-plength(::Type{T}) where {T <: Floats} = 2
-plength(::Type{T}) where {T <: Union{Val{'e'}, Val{'E'}}} = 4
-plength(::Type{T}) where {T} = 0
+@inline function computelen(substringranges, formats, args)
+    # len = sum(length, substringranges)
+    # for (f, arg) in zip(formats, args)
+    #     if ischar(f.type)
+    #         len += max(f.width, 1) + (ncodeunits(arg isa String ? arg[1] : Char(arg)) - 1)
+    #     elseif isstring(f.type)
+    #         str = string(arg)
+    #         p = f.precision == -1 ? (length(str) + (f.hash ? (arg isa Symbol ? 1 : 2) : 0)) : f.precision
+    #         len += max(f.width, p) + (sizeof(str) - length(str))
+    #     elseif f.type == 'p'
+    #         len += max(f.width, 2 * sizeof(arg) + 2)
+    #     elseif isint(f.type)
+    #         arg2 = arg isa AbstractFloat ? Integer(trunc(arg)) : arg
+    #         y = max(f.width, f.precision + ndigits(arg2, base=base(f.type), pad=1) + plength(f.type))
+    #         len += y
+    #     elseif arg isa BigFloat
+    #         y = Base.MPFR._calculate_buffer_size!(Base.StringVector(0), string(f), arg) + 3
+    #         len += y
+    #     elseif isfloat(f.type)
+    #         len += max(f.width, f.precision + plength(arg) + f.hash + plength(f.type))
+    #     else
+    #         throw(ArgumentError("invalid format type specifier: $f"))
+    #     end
+    # end
+    # return len
+    return 10
+end
 
 plength(x::Float16) = 9 + 5
 plength(x::Float32) = 39 + 9
-plength(x::Union{Float64, BigFloat}) = 309 + 17
-plength(x::Real) = plength(float(x))
-plength(x::Integer) = ndigits(x, base=10)
-plength(c::Char) = ncodeunits(c)
-plength(s::AbstractString) = sizeof(s)
-plength(p::Ptr) = 2 * sizeof(p) + 2
-plength(x) = 10
-
-@inline function preallocate(f, args...)
-    len = sum(length, f.substrings)
-    N = length(f.formats)
-    # unroll up to 8 formats
-    Base.@nexprs 8 i -> begin
-        if N >= i
-            len += plength(f.formats[i], args[i])
-        end
-    end
-    if N > 8
-        for i = 9:length(f.formats)
-            len += plength(f.formats[i], args[i])
-        end
-    end
-    return len
-end
+plength(x) = 309 + 17
+plength(c::Char) = (c == 'e' || c == 'E') ? 4 : (c == 'a' || c == 'A' ? 5 : 2)
 
 @noinline argmismatch(a, b) =
     throw(ArgumentError("mismatch between # of format specifiers and provided args: $a != $b"))
 
 function format(io::IO, f::Format, args...) # => Nothing
     length(args) == length(f.formats) || argmismatch(length(args), length(f.formats))
-    buf = Vector{UInt8}(undef, preallocate(f, args...))
-    pos = format(buf, 1, f, args...)
-    GC.@preserve buf unsafe_write(io, pointer(buf), pos - 1)
+    buf = Base.StringVector(computelen(f.substringranges, f.formats, args))
+    pos = format(buf, 1, f, args)
+    write(io, resize!(buf, pos - 1))
     return
 end
 
 function format(f::Format, args...) # => String
     length(args) == length(f.formats) || argmismatch(length(args), length(f.formats))
-    buf = Vector{UInt8}(undef, preallocate(f, args...))
-    pos = format(buf, 1, f, args...)
-    return unsafe_string(pointer(buf), pos-1)
+    buf = Base.StringVector(computelen(f.substringranges, f.formats, args))
+    pos = format(buf, 1, f, args)
+    return String(resize!(buf, pos - 1))
 end
 
 macro printf(io_or_fmt, args...)
